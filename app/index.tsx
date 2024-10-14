c076562bedd69f9c425f77d2a4f37129660aee4c
etienne <belemgnegreetienne@gmail.com>
2024-10-14 12:07:12 +0000

    feat: traduction du design

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const colors = useThemeColors();
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView style={[styles.container, { backgroundColor: colors.tint }]}>
+      <ThemedText variant="headline" color="grayWhite">
+        Pokédex
+      </ThemedText>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { backgroundColor: "#ff0000", flex: 1 },
+});
